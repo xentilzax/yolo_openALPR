27b9c284ca094fb1ec097797e99a6baddac21a70
Denis Kravchenko <xentilzax@gmail.com>
2018-08-09 14:58:39 +0300

    make script build install package

--- build.sh
+++ build.sh
@@ -8,3 +8,12 @@ cp -f darknet.so ../lp_recognizer/
 cd ../lp_recognizer
 make clean
 make
+cd ..
+
+rm -rf build
+mkdir -p build
+cp -r ./darknet/weights ./build/
+cp -r ./darknet/cfg ./build/
+cp -r ./lp_recognizer/cfg ./build/
+cp ./lp_recognizer/darknet.so ./build/
+cp ./lp_recognizer/lprecognazer ./build/
--- build_deb_package.sh
+++ build_deb_package.sh
@@ -3,47 +3,44 @@
 set -eux
 
 
-#apt-get install ruby-dev build-essential
-#gem install fpm
+sudo apt-get install ruby-dev build-essential
+sudo gem install fpm
 
 #
-# Собирает DEB-пакет для inex_LPR 
+# Собирает DEB-пакет для inex license plate recognazer 
 #
-NAME="lpr"
-BRANCH=${BRANCH:-master}
+COMPANY="inex"
+NAME="lpreconizer"
+BRANCH=${1:-"master"}
 VERSION="1.0"
 DESCRIPTION="License plate recognizer"
 WORKSPACE=$PWD
 
 echo "branch = $BRANCH"
-#mkdir -p inex
-#cd inex
 
-#git clone https://github.com/xentilzax/yolo_openALPR.git
 git checkout $BRANCH
 
 source ./build.sh
 
-RELEASE_DIR=$PWD/lp_recognizer 
+RELEASE_DIR=$PWD/build 
 
 # Считываем метаданные приложения
 # NAME, VERSION, DESCRIPTION
 #source "$SOURCE_DIR/head_detection/application/queue_detection_application/build/metadata.sh"
 
-if ["$BRANCH" == "master"]; then
-    PACKEGE_NAME="$NAME"
-else
-    PACKEGE_NAME="$NAME-$BRANCH"
+if [ "$BRANCH" = "master" ]; then   
+  PACKEGE_NAME="$COMPANY-$NAME"
+else 
+  PACKEGE_NAME="$COMPANY-$NAME-$BRANCH"
 fi
 
 echo "package name = $PACKEGE_NAME"
-
-rm -rf $PACKEGE_NAME.deb
 
 fpm -f -t deb -s dir -C "$RELEASE_DIR" \
     --name "$PACKEGE_NAME" \
     --version "$VERSION" \
     --description "$DESCRIPTION" \
+    --prefix "/opt/$COMPANY/$NAME" \
+    -p $WORKSPACE
 #    --iteration "$(utils::iteration $SOURCE_DIR $BUILD_NUMBER)" \
 #    --after-install "$CWD/postinst.sh" \
-    --prefix "/opt/inex/lpr/$NAME" -p $WORKSPACE
