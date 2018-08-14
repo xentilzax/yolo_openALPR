bb77a2e3c40e3182e97fa9b58162053819ca6e1e
Denis Kravchenko <xentilzax@gmail.com>
2018-08-14 18:55:23 +0300

    add one more symbol link to register service script

--- lp_recognizer/register_service.sh
+++ lp_recognizer/register_service.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 ln -s /opt/inex/lprecognizer/cfg/lprecognizer.service /etc/systemd/system/lprecognizer.service
+ln -s /etc/systemd/system/lprecognizer.service /etc/systemd/system/multi-user.target.wants/lprecognizer.service
