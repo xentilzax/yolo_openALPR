de8e28119748afcfb45f230326d529f64de4884c
Denis Kravchenko <xentilzax@gmail.com>
2018-08-14 18:17:52 +0300

    add script creating soft link for service register

--- build.sh
+++ build.sh
@@ -15,5 +15,7 @@ mkdir -p build
 cp -r ./darknet/weights ./build/
 cp -r ./darknet/cfg ./build/
 cp -r ./lp_recognizer/cfg ./build/
+cp -r ./lp_recognizer/register_service.sh ./build/
+
 cp ./darknet/nn.so ./build/
 cp ./lp_recognizer/lprecognizer ./build/
