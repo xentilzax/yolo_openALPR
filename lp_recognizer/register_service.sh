0861e384db1d8d7873d3354982775b1319a8f158
Denis Kravchenko <xentilzax@gmail.com>
2018-08-14 19:59:57 +0300

    fix bug register service

--- lp_recognizer/register_service.sh
+++ lp_recognizer/register_service.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-ln -s /opt/inex/lprecognizer/cfg/lprecognizer.service /etc/systemd/system/lprecognizer.service
-ln -s /etc/systemd/system/lprecognizer.service /etc/systemd/system/multi-user.target.wants/lprecognizer.service
+#ln -s /opt/inex/lprecognizer/cfg/lprecognizer.service /etc/systemd/system/lprecognizer.service
+#ln -s /etc/systemd/system/lprecognizer.service /etc/systemd/system/multi-user.target.wants/lprecognizer.service
+
+systemctl enable /opt/inex/lprecognizer/cfg/lprecognizer.service
+#systemctl daemon-reload
+#systemctl enable lprecognizer.service
