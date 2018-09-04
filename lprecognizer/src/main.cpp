b0a2a2c571f5e64a2f8a6202f21833b73bdad65b
Denis Kravchenko <denis_e-work@mail.ru>
2018-09-05 02:29:54 +0300

    add feature: print results to ALPR json format

--- lprecognizer/src/main.cpp
+++ lprecognizer/src/main.cpp
@@ -13,13 +13,28 @@
 #include "common.hpp"
 #include "cJSON.h"
 #include "post.hpp"
+//---------------------------------------------------------------------------------------------------------------
 
 #define AVR_W 0.1
+#define VERSION 1.0
+#define SITE_ID "INEX"
+
+//---------------------------------------------------------------------------------------------------------------
+namespace Inex
+{
+typedef alpr::AlprResults Results;
+typedef alpr::AlprRegionOfInterest RegionOfInterest;
+}
+//---------------------------------------------------------------------------------------------------------------
 
 Config cfg;
 
 //---------------------------------------------------------------------------------------------------------------
 int ParseConfig(const std::string & str, Config & cfg);
+std::string ResultToJsonString(const Inex::Results & results,
+                               const cv::Mat & img,
+                               const std::vector<Inex::RegionOfInterest> & roi_list,
+                               unsigned int camera_id);
 
 //---------------------------------------------------------------------------------------------------------------
 int main(int argc, char *argv[])
@@ -114,17 +129,15 @@ int main(int argc, char *argv[])
 
             //read the current frame
             clock_t begin_time = clock();
-            clock_t end_time, yolo_time, alpr_time, begin_yolo, begin_alpr, end_yolo, end_alpr;
+            clock_t end_time, begin_yolo, begin_alpr, end_yolo, end_alpr;
             float yolo_cur_dtime = 0;
             float alpr_cur_dtime = 0;
-
-
 
             count_images++;
 
             std::vector<bbox_t> result_vec;
             std::vector<alpr::AlprRegionOfInterest> roi_list;
-            std::vector<alpr::AlprResults> results;
+            alpr::AlprResults results;
 
             if ( cfg.use_yolo_detector ) { // use YOLO detector LP
                 begin_yolo = clock();
@@ -141,10 +154,11 @@ int main(int argc, char *argv[])
                     img(cv::Rect(b.x, b.y, b.w, b.h)).copyTo(img_roi);
 
                     // Recognize an image file. Alternatively, you could provide the image bytes in-memory.
-		    alpr::AlprResults result;
-                    result = openalpr.recognize((unsigned char*)(img_roi.data), img_roi.channels(), img_roi.cols, img_roi.rows, roi_list);
-                    results.push_back(result);
+                    alpr::AlprResults roi_result;
+                    roi_result = openalpr.recognize((unsigned char*)(img_roi.data), img_roi.channels(), img_roi.cols, img_roi.rows, roi_list);
+                    results.plates.insert( results.plates.end(), roi_result.plates.begin(), roi_result.plates.end() );
                 }//for
+
                 end_alpr = clock();
                 alpr_cur_dtime = float(end_alpr - begin_alpr) / CLOCKS_PER_SEC;
 
@@ -155,23 +169,21 @@ int main(int argc, char *argv[])
                     }
                 }
             } else { //use Open_ALPR detector LP
-                alpr::AlprResults result;
+
                 begin_alpr = clock();
-
-                result = openalpr.recognize((unsigned char*)(img.data), img.channels(), img.cols, img.rows, roi_list);
-
+                results = openalpr.recognize((unsigned char*)(img.data), img.channels(), img.cols, img.rows, roi_list);
                 end_alpr = clock();
                 alpr_cur_dtime = float(end_alpr - begin_alpr) / CLOCKS_PER_SEC;
-                results.push_back(result);
+
 
                 if ( cfg.gui_enable ) {
                     cv::Point points[4];
                     const cv::Point* pts[1] = {points};
                     int npts[1] = {4};
-                    for(size_t i = 0; i < result.plates.size(); i++) {
+                    for(size_t i = 0; i < results.plates.size(); i++) {
                         for(size_t j = 0; j < 4; j++) {
-                            points[j].x = result.plates[i].plate_points[j].x;
-                            points[j].y = result.plates[i].plate_points[j].y;
+                            points[j].x = results.plates[i].plate_points[j].x;
+                            points[j].y = results.plates[i].plate_points[j].y;
                         }
                         cv::polylines(img, pts, npts, 1, true, cv::Scalar(0, 0, 255), 2);
                     }
@@ -181,42 +193,28 @@ int main(int argc, char *argv[])
             end_time =clock();
 
             if ( cfg.verbose_level >= 2 ) {
-                for (size_t j = 0; j < results.size(); j++) {
-                    // Carefully observe the results. There may be multiple plates in an image,
-                    // and each plate returns the top N candidates.
-                    for (size_t i = 0; i < results[j].plates.size(); i++)
-                    {
-                        alpr::AlprPlateResult plate = results[j].plates[i];
-                        if( plate.topNPlates.size() > 0) {
-                            alpr::AlprPlate candidate = plate.topNPlates[0];
-                            std::cout << " plate " << i << " : " << candidate.characters << std::endl;
-                        } else {
-                            std::cout << " Not found license plates" << std::endl;
-                        }
-                    }
+                // Carefully observe the results. There may be multiple plates in an image,
+                // and each plate returns the top N candidates.
+                for (size_t i = 0; i < results.plates.size(); i++)
+                {
+                    std::cout << " plate " << i << " : " << results.plates[i].bestPlate.characters << std::endl;
                 }
+
             }
 
-            count_found_LP = 0;
+            count_found_LP = results.plates.size();
             count_recognize_LP = 0;
-            for (size_t j = 0; j < results.size(); j++) {
-                if ( cfg.use_yolo_detector ) {
-                    count_found_LP++;
-                }
-                for (size_t i = 0; i < results[j].plates.size(); i++)
-                {
-                    if ( !cfg.use_yolo_detector ) {
-                        count_found_LP++;
-                    }
-                    alpr::AlprPlateResult plate = results[j].plates[i];
-                    if ( plate.topNPlates.size() > 0) {
-                        count_recognize_LP++;
-                        std::string jsonResults = alpr::Alpr::toJson(results[j].plates[i]);
 
-                        if (!PostHTTP(cfg.server, jsonResults)) {
-                            fprintf(stderr, "Fatal: PostHTTP failed.\n");
-                            return EXIT_FAILURE;
-                        }
+            for (size_t i = 0; i < results.plates.size(); i++)
+            {
+                alpr::AlprPlateResult plate = results.plates[i];
+                if ( plate.topNPlates.size() > 0) {
+                    count_recognize_LP++;
+                    std::string jsonResults = alpr::Alpr::toJson(results.plates[i]);
+
+                    if (!PostHTTP(cfg.server, jsonResults)) {
+                        fprintf(stderr, "Fatal: PostHTTP failed.\n");
+                        return EXIT_FAILURE;
                     }
                 }
             }
@@ -253,11 +251,6 @@ int main(int argc, char *argv[])
 
         }//while
         capture.release();
-    }
-
-    if ( cfg.verbose_level >= 1 ) {
-        std::cout << "\n==========================\n";
-        std::cout << "total images: "<< count_images << std::endl;
     }
 }
 
@@ -377,4 +370,195 @@ end:
 
     cJSON_Delete(json);
     return status;
+}
+
+//---------------------------------------------------------------------------------------------------------------
+/*
+ *
+ *{
+  "version": 2,
+  "data_type": "alpr_results",
+  "epoch_time": 1490574589596,
+  "img_width": 1280,
+  "img_height": 720,
+  "processing_time_ms": 259.132385,
+  "error": false,
+  "regions_of_interest": [
+    {
+      "x": 60,
+      "y": 600,
+      "width": 620,
+      "height": 120
+    },
+    {
+      "x": 0,
+      "y": 0,
+      "width": 1280,
+      "height": 570
+    }
+  ],
+  "results": [
+  {
+  "plate": "4PEB029",
+  "confidence": 94.94176483154297,
+  "matches_template": 1,
+  "plate_index": 0,
+  "region": "us-ca",
+  "region_confidence": 99,
+  "processing_time_ms": 16.775239944458008,
+  "requested_topn": 5,
+  "coordinates": [
+    {
+      "x": 0,
+      "y": 0
+    },
+    {
+      "x": 203,
+      "y": 0
+    },
+    {
+      "x": 203,
+      "y": 85
+    },
+    {
+      "x": 0,
+      "y": 85
+    }
+  ],
+  "vehicle_region": {
+    "x": 59,
+    "y": 1,
+    "width": 84,
+    "height": 84
+  },
+  "candidates": [
+    {
+      "plate": "4PEB029",
+      "confidence": 94.94176483154297,
+      "matches_template": 1
+    },
+    {
+      "plate": "4PEB29",
+      "confidence": 81.52086639404297,
+      "matches_template": 0
+    },
+    {
+      "plate": "4PE029",
+      "confidence": 81.5196762084961,
+      "matches_template": 0
+    },alpr::Alpr
+    {
+      "plate": "4PB029",
+      "confidence": 81.51945495605469,
+      "matches_template": 0
+    },
+    {
+      "plate": "4PEBO29",
+      "confidence": 81.39118957519531,
+      "matches_template": 0
+    }
+  ],
+  "id": 101
+}
+  ],
+  "uuid": "unspecified-cam16488027-1490574589596",
+  "camera_id": 16488027,
+  "agent_uid": "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY",
+  "agent_version": "2.3.111",
+  "agent_type": "alprd",
+  "site_id": "unspecified",
+  "company_id": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
+ * */
+
+std::string ResultToJsonString(const Inex::Results & results,
+                               const cv::Mat & img,
+                               const std::vector<Inex::RegionOfInterest> & roi_list,
+                               unsigned int camera_id)
+{
+    static unsigned int id = 0;
+    cJSON * json_root = cJSON_CreateObject();
+
+    cJSON_AddItemToObject(json_root, "version", cJSON_CreateNumber(VERSION));
+    cJSON_AddItemToObject(json_root, "epoch_time", cJSON_CreateString( std::to_string(results.epoch_time).c_str()));
+    cJSON_AddItemToObject(json_root, "img_width", cJSON_CreateNumber( img.cols));
+    cJSON_AddItemToObject(json_root, "img_height", cJSON_CreateNumber( img.rows));
+    cJSON_AddItemToObject(json_root, "processing_time", cJSON_CreateNumber( results.total_processing_time_ms));
+    cJSON_AddItemToObject(json_root, "error", cJSON_CreateBool( results.error));
+
+    cJSON * json_regions_of_interest = cJSON_CreateArray();
+
+    for(size_t i = 0; i < roi_list.size(); i++) {
+        cJSON * json_item_array = cJSON_CreateObject();
+
+        cJSON_AddItemToObject(json_item_array, "x", cJSON_CreateNumber( roi_list[i].x));
+        cJSON_AddItemToObject(json_item_array, "y", cJSON_CreateNumber( roi_list[i].y));
+        cJSON_AddItemToObject(json_item_array, "width", cJSON_CreateNumber( roi_list[i].width));
+        cJSON_AddItemToObject(json_item_array, "height", cJSON_CreateNumber( roi_list[i].height));
+
+        cJSON_AddItemToArray(json_regions_of_interest, json_item_array);
+    }
+
+    cJSON_AddItemToObject(json_root, "regions_of_interest", json_regions_of_interest);
+
+    cJSON * json_results = cJSON_CreateArray();
+    for (size_t j = 0; j < results.plates.size(); j++) {
+        cJSON * json_item_array = cJSON_CreateObject();
+
+        cJSON_AddItemToObject(json_item_array, "plate", cJSON_CreateString( results.plates[j].bestPlate.characters.c_str() ));
+        cJSON_AddItemToObject(json_item_array, "confidence", cJSON_CreateNumber( results.plates[j].bestPlate.overall_confidence));
+        cJSON_AddItemToObject(json_item_array, "matches_template", cJSON_CreateBool( results.plates[j].bestPlate.matches_template));
+        cJSON_AddItemToObject(json_item_array, "plate_index", cJSON_CreateNumber(j));
+        cJSON_AddItemToObject(json_item_array, "region", cJSON_CreateString( results.plates[j].region.c_str() ));
+        cJSON_AddItemToObject(json_item_array, "region_confidence", cJSON_CreateNumber( results.plates[j].regionConfidence));
+        cJSON_AddItemToObject(json_item_array, "processing_time_ms", cJSON_CreateNumber( results.plates[j].processing_time_ms));
+        cJSON_AddItemToObject(json_item_array, "requested_topn", cJSON_CreateNumber( results.plates[j].requested_topn));
+
+        cJSON * json_coords = cJSON_CreateArray();
+        for (size_t k = 0; k < 4; k++) {
+            cJSON * json_point = cJSON_CreateObject();
+            cJSON_AddItemToObject(json_point, "x", cJSON_CreateNumber( results.plates[j].plate_points[k].x));
+            cJSON_AddItemToObject(json_point, "y", cJSON_CreateNumber( results.plates[j].plate_points[k].y));
+            cJSON_AddItemToArray(json_coords, json_point);
+        }
+        cJSON_AddItemToObject(json_item_array, "coordinates", json_coords);
+
+        cJSON * json_vehicle_region = cJSON_CreateObject();
+        {
+            cJSON_AddItemToObject(json_vehicle_region, "x", cJSON_CreateNumber( results.plates[j].vehicle_region.x));
+            cJSON_AddItemToObject(json_vehicle_region, "y", cJSON_CreateNumber( results.plates[j].vehicle_region.y));
+            cJSON_AddItemToObject(json_vehicle_region, "width", cJSON_CreateNumber( results.plates[j].vehicle_region.width));
+            cJSON_AddItemToObject(json_vehicle_region, "height", cJSON_CreateNumber( results.plates[j].vehicle_region.height));
+        }
+        cJSON_AddItemToObject(json_item_array, "vehicle_region", json_vehicle_region);
+
+        cJSON * json_candidates = cJSON_CreateArray();
+        for (size_t k = 0; k < 4; k++) {
+            cJSON * json_item = cJSON_CreateObject();
+            cJSON_AddItemToObject(json_item, "plate", cJSON_CreateString( results.plates[j].topNPlates[k].characters.c_str() ));
+            cJSON_AddItemToObject(json_item, "confidence", cJSON_CreateNumber( results.plates[j].topNPlates[k].overall_confidence));
+            cJSON_AddItemToObject(json_item, "matches_template", cJSON_CreateBool( results.plates[j].topNPlates[k].overall_confidence));
+            cJSON_AddItemToArray(json_candidates, json_item);
+        }
+        cJSON_AddItemToObject(json_item_array, "coordinates", json_coords);
+
+        cJSON_AddItemToObject(json_item_array, "id", cJSON_CreateNumber(id));
+
+        cJSON_AddItemToArray(json_results, json_item_array);
+    }
+    cJSON_AddItemToObject(json_root, "results", json_results);
+
+    std::string uuid = std::string(SITE_ID) + "-cam" + std::to_string(camera_id) + "-" + std::to_string(results.epoch_time);
+
+    cJSON_AddItemToObject(json_root, "uuid", cJSON_CreateString( uuid.c_str() ));
+    cJSON_AddItemToObject(json_root, "camera_id", cJSON_CreateString( std::to_string(camera_id).c_str() ));
+    cJSON_AddItemToObject(json_root, "site_id", cJSON_CreateString( SITE_ID ));
+
+    std::string str = cJSON_Print(json_root);
+
+    cJSON_Delete(json_root);
+
+    id++;
+
+    return str;
 }
